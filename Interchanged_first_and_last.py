5731fe877f4103914c8e33e09738a6a32cf9e9df
chinmaykambli147@gmail.com <chinmaykambli147@gmail.com>
2024-06-08 17:41:32 +0530

    5:41 latest

--- Interchanged_first_and_last.py
+++
@@ -1,9 +0,0 @@
-str1 = [10, 20, 30, 40]
-print(f"Before Interchange :", str1)
-size = len(str1)
-temp = str1[0]
-str1[0] = str1[size - 1]
-str1[size - 1] = temp
-print(f"After Interchanged :", str1)
-
-
--- Reverse_string.py
+++
@@ -1,6 +0,0 @@
-Name = " Ronaldo "
-print(f"Before reverse :", Name)
-string1 = " "
-for char in Name[::-1]:
-    string1 = string1 + char
-print(f"After reverse :", string1)
