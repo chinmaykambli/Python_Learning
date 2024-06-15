22a960b368adac1a16a180896128720d487e2b61
Chinmay Kambli <145270367+chinmaykambli@users.noreply.github.com>
2024-06-15 18:11:09 +0530

    Update string_2.py

--- 15_06_24/string_2.py
+++ 15_06_24/string_2.py
@@ -1,3 +1,10 @@
+#program to get a string made of the first 2 and last 2 characters of a given string. If the string length is less than 2, return the empty string instead.
+#Sample String : 'w3resource'
+#Expected Result : 'w3ce'
+#Sample String : 'w3'
+#Expected Result : 'w3w3'
+#Sample String : ' w'
+#Expected Result : Empty String
 def len_of_fun(str1):
     if len(str1) < 2:
         return ''
